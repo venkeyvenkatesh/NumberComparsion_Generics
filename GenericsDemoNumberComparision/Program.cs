0963ba316bc6e2a4c8ffb465eaa37eac9e97b571
venkey <venkeyvenkatesh885@gmail.com>
2020-10-20 16:11:17 +0530

    [venkey]Add.getMaximum method to find the maximum of three Float values

--- GenericsDemoNumberComparision/Program.cs
+++ GenericsDemoNumberComparision/Program.cs
@@ -12,13 +12,26 @@ namespace GenericsDemoNumberComparision
             Console.WriteLine("Welcome to Generics Demo Program");
 
             Console.WriteLine("Enter three Integers");
-            int a = Convert.ToInt32(Console.ReadLine());
-            int b = Convert.ToInt32(Console.ReadLine());
-            int c = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("max of three numbers is :" + getMaximum(a, b, c));
+            int firstInt = Convert.ToInt32(Console.ReadLine());
+            int secondInt= Convert.ToInt32(Console.ReadLine());
+            int thirdInt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("max of three numbers is :" + getMaximum(firstInt,secondInt,thirdInt));
+
+            Console.WriteLine("Enter three FLoat values");
+            double firstDouble = Convert.ToDouble(Console.ReadLine());
+            double secondDouble = Convert.ToDouble(Console.ReadLine());
+            double thirdDouble = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("max of three numbers is :" + getMaximum(firstDouble,secondDouble,thirdDouble));
+
+
 
         }
         private static int getMaximum(int a, int b, int c)
+        {
+            return a.CompareTo(b) >= 0 && a.CompareTo(c) >= 0 ? a : b.CompareTo(a) >= 0 && b.CompareTo(c) >= 0 ? b : c;
+
+        }
+        private static double getMaximum(double a, double b, double c)
         {
             return a.CompareTo(b) >= 0 && a.CompareTo(c) >= 0 ? a : b.CompareTo(a) >= 0 && b.CompareTo(c) >= 0 ? b : c;
 
