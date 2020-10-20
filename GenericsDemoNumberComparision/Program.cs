d4580afaa34e750e1855ef66fd29b76f01758915
venkey <venkeyvenkatesh885@gmail.com>
2020-10-20 15:59:32 +0530

    First  commit

---
+++ GenericsDemoNumberComparision/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data;
+using System.Reflection.Metadata.Ecma335;
+
+namespace GenericsDemoNumberComparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Generics Demo Program");
+        }
+    }
+}
+
